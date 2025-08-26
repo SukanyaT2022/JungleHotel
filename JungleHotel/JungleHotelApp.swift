1c4d39bd80596f3fd0b68f519f6693763141a8c3
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2025-08-26 11:09:52 -0400

    Initial Commit

---
+++ JungleHotel/JungleHotelApp.swift
@@ -0,0 +1,17 @@
+//
+//  JungleHotelApp.swift
+//  JungleHotel
+//
+//  Created by TS2 on 8/26/25.
+//
+
+import SwiftUI
+
+@main
+struct JungleHotelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JungleHotelTests/JungleHotelTests.swift
@@ -0,0 +1,17 @@
+//
+//  JungleHotelTests.swift
+//  JungleHotelTests
+//
+//  Created by TS2 on 8/26/25.
+//
+
+import Testing
+@testable import JungleHotel
+
+struct JungleHotelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
